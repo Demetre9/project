ae49c51ca63e6029459be9c9a2e5df51cf4a5802
Demetre Pilpani <demetre.pilpani@gmail.com>
2023-02-04 13:04:22 +0400

    modal and sig up

---
+++ signup.js
@@ -0,0 +1,43 @@
+// Get input fields
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const submitBtn = document.getElementById("submitBtn");
+
+// Validate username
+username.addEventListener("input", function () {
+  if (username.value.length >= 3) {
+    username.setCustomValidity("");
+  } else {
+    username.setCustomValidity("Username must be at least 3 characters long");
+  }
+});
+
+// Validate password
+password.addEventListener("input", function () {
+  if (password.value.length >= 6) {
+    let upperCaseLetters = /[A-Z]/g;
+    if (password.value.match(upperCaseLetters)) {
+      let uppercaseCount = password.value.match(upperCaseLetters).length;
+      if (uppercaseCount >= 2) {
+        password.setCustomValidity("");
+      } else {
+        password.setCustomValidity(
+          "Password must contain at least 2 uppercase characters"
+        );
+      }
+    } else {
+      password.setCustomValidity(
+        "Password must contain at least 2 uppercase characters"
+      );
+    }
+  } else {
+    password.setCustomValidity("Password must be at least 6 characters long");
+  }
+});
+
+// Submit form only if inputs are valid
+submitBtn.addEventListener("click", function () {
+  if (username.checkValidity() && password.checkValidity()) {
+    alert("Form Submitted Successfully!");
+  }
+});
