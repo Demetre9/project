f770de1864e0353bc6b7628b7a183e59f68e2cdf
Demetre Pilpani <demetre.pilpani@gmail.com>
2023-02-06 14:45:57 -0800

    add new modale

--- modal.js
+++ modal.js
@@ -2,6 +2,10 @@
 const loginBtn = document.getElementById("username-login");
 const usernameInput = document.getElementById("username");
 const closeLoginBtn = document.getElementById("closeLoginBtn");
+const password = document.getElementById("password");
+const submitBtn = document.getElementById("submitBtn");
+const signupPassword = document.getElementById("signupPassword");
+const signUpusername = document.getElementById("signUpusername");
 
 // Show the login modal when the user clicks on the "Login" button
 loginBtn.addEventListener("click", () => {
@@ -40,6 +44,58 @@ document.forms[1].addEventListener("submit", (event) => {
   const signupUsername = document.getElementById("signup-username").value;
   document.getElementById("username-signup").innerHTML = signupUsername;
   signupModal.style.display = "none";
+});
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    document.getElementById("username-signup").style.display = "none";
+  });
+// Validate username
+usernameInput.addEventListener("input", function () {
+  if (usernameInput.value.length >= 3) {
+    usernameInput.setCustomValidity("");
+  } else {
+    usernameInput.setCustomValidity(
+      "Username must be at least 3 characters long"
+    );
+  }
+});
+// Validate password
+
+signupPassword.addEventListener("input", function () {
+  if (signupPassword.value.length >= 6) {
+    let upperCaseLetters = /[A-Z]/g;
+    if (signupPassword.value.match(upperCaseLetters)) {
+      let uppercaseCount = signupPassword.value.match(upperCaseLetters).length;
+      if (uppercaseCount >= 2) {
+        signupPassword.setCustomValidity("");
+        document.forms[1].addEventListener("submit", (event) => {
+          event.preventDefault();
+          loginBtn.innerHTML = "Welcome " + signUpusername.value;
+          document.getElementById("signupModal").style.display = "none";
+          closeSignupBtn.click();
+        });
+      }
+    } else {
+      signupPassword.setCustomValidity(
+        "Password must contain at least 2 uppercase characters"
+      );
+    }
+  } else {
+    signupPassword.setCustomValidity(
+      "Password must be at least 6 characters long"
+    );
+  }
+});
+
+document.getElementById("signup-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  loginBtn.innerHTML = "Welcome " + usernameInput.value;
+  document.getElementById("username-signup").style.display = "none";
+
+  document.getElementById("username-signup").style.display = "none";
 });
 
 document
--- sign up/signup.js
+++ sign up/signup.js
@@ -1,17 +1,15 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const submitBtn = document.getElementById("submitBtn");
-
 // Validate username
-username.addEventListener("input", function () {
-  if (username.value.length >= 3) {
-    username.setCustomValidity("");
+usernameInput.addEventListener("input", function () {
+  if (usernameInput.value.length >= 3) {
+    usernameInput.setCustomValidity("");
   } else {
-    username.setCustomValidity("Username must be at least 3 characters long");
+    usernameInput.setCustomValidity(
+      "Username must be at least 3 characters long"
+    );
   }
 });
-
 // Validate password
+
 password.addEventListener("input", function () {
   if (password.value.length >= 6) {
     let upperCaseLetters = /[A-Z]/g;
@@ -19,6 +17,12 @@ password.addEventListener("input", function () {
       let uppercaseCount = password.value.match(upperCaseLetters).length;
       if (uppercaseCount >= 2) {
         password.setCustomValidity("");
+        document.forms[1].addEventListener("submit", (event) => {
+          event.preventDefault();
+          loginBtn.innerHTML = "Welcome " + signupUsername.value;
+          document.getElementById("signupModal").style.display = "none";
+          closeSignupBtn.click();
+        });
       } else {
         password.setCustomValidity(
           "Password must contain at least 2 uppercase characters"
@@ -34,9 +38,18 @@ password.addEventListener("input", function () {
   }
 });
 
-submitBtn.addEventListener("click", function () {
-  if (username.checkValidity() && password.checkValidity()) {
-    alert("Form Submitted Successfully!");
-    window.location.replace("index.html");
-  }
+// // When the form is submitted, update the text on the page to show the username
+// document.forms[1].addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const signupUsername = document.getElementById("signup-username").value;
+//   document.getElementById("username-signup").innerHTML = signupUsername;
+// });
+
+// When the form is submitted, update the text of the "Login" button to show the username
+document.getElementById("signup-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  loginBtn.innerHTML = "Welcome " + usernameInput.value;
+  document.getElementById("loginModal").style.display = "none";
+
+  document.getElementById("signupModall").style.display = "none";
 });
